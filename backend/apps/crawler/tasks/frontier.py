917e86e009ec6181feb5675084b7a1e30a83232e
rebornbd <dipta.dhar.bd@gmail.com>
2021-07-24 14:35:33 +0600

    frontier init

--- backend/apps/crawler/tasks/frontier.py
+++ backend/apps/crawler/tasks/frontier.py
@@ -1,2 +1,39 @@
-from celery import shared_task
+# from celery import shared_task
 
+
+class URLfrontier():
+    def __init__(self, urls=[]):
+        if type(urls) != type([]):
+            urls = []
+        self.urls = urls
+    
+    # urls prioritizer
+    # parameters: 
+    # return: 
+    def prioritizer(self):
+        pass
+
+    # front queue selector & back queue router & maintains prioritization
+    # parameters: 
+    # return: 
+    def frontQSelector(self):
+        pass
+    
+    # back queue selector & maintains politeness
+    # parameters: 
+    # return: 
+    def backQSelector(self):
+        pass
+    
+    
+    """
+        frontier all dependency methods
+        are implement below
+    """
+    # method name
+    pass
+
+
+uf = URLfrontier('')
+
+print(type(uf.urls))
